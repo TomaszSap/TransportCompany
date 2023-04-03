2fcd9ae185b799f9e4fd2c9b490924a9feb3d9dc
sapol <sapolek@gmail.com>
2023-04-03 11:09:53 +0200

    EmployeeService modification

--- src/main/java/com/example/TransportCompany/services/EmployeeService.java
+++ src/main/java/com/example/TransportCompany/services/EmployeeService.java
@@ -1,9 +1,9 @@
 package com.example.TransportCompany.services;
 
-import com.example.TransportCompany.model.Course;
 import com.example.TransportCompany.model.Employee;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 public interface EmployeeService {
 
@@ -11,10 +11,11 @@ public interface EmployeeService {
     abstract void deleteCourseFromTable(Employee employee,int id);
     abstract boolean addEmployee(Employee employee);
     abstract void changeRole(int employeeId, String role);
-    abstract void deleteEmployee(int employeeId);
+    abstract boolean deleteEmployee(int employeeId);
     abstract boolean addCourse(int courseId, HttpSession httpSession);
     abstract boolean deleteCourse(int employeeId,int courseId);
     abstract boolean assignCar(int employeeId,int carId);
     abstract boolean unassignCar(int employeeId,int carId);
+    abstract List<Employee> getAllEmployers();
 
 }
