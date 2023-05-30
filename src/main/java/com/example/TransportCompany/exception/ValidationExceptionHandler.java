e8b8d56f54697cd2e8a9ea192703c2e32dfdfd62
sapol <sapolek@gmail.com>
2023-05-30 13:30:42 +0200

    moving class ValidationExceptionHandler.java to com.example.TransportCompany.exception package

--- src/main/java/com/example/TransportCompany/exception/ValidationExceptionHandler.java
+++ src/main/java/com/example/TransportCompany/exception/ValidationExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.TransportCompany.config;
+package com.example.TransportCompany.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
