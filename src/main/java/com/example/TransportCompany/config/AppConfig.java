ceeea6c21c4fb365c1ed39ab979d0ec1440ac494
sapol <sapolek@gmail.com>
2023-06-20 11:24:10 +0200

    Using RestTemplate instead raw connection

--- src/main/java/com/example/TransportCompany/config/AppConfig.java
+++ src/main/java/com/example/TransportCompany/config/AppConfig.java
@@ -1,16 +1,19 @@
 package com.example.TransportCompany.config;
 
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- src/main/java/com/example/TransportCompany/services/OpenRouteService.java
+++ src/main/java/com/example/TransportCompany/services/OpenRouteService.java
@@ -1,80 +1,85 @@
 package com.example.TransportCompany.services;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Optional;
-import java.util.Scanner;
 
 @Service
 public class OpenRouteService {
     @Value("${open_route_key}")
     private String API_KEY;
 
-    private Optional<JSONArray> getCoordinates(String city) throws JSONException {
-        JSONArray coordinates = null;
-        try {
-            URL url = new URL("https://api.openrouteservice.org/geocode/search?api_key=" + API_KEY + "&text=" + city);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
+    @Autowired
+    private RestTemplate restTemplate;
 
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-            }
 
-            Scanner scanner = new Scanner(url.openStream());
-            String response = scanner.useDelimiter("\\Z").next();
+    public Optional<Map<String, Object>> getCoordinates(String city) {
+        String url = "https://api.openrouteservice.org/geocode/search?api_key=" + API_KEY + "&text=" + city;
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        RequestEntity<?> requestEntity = new RequestEntity<>(headers, HttpMethod.GET, URI.create(url));
+        var responseEntity = restTemplate.exchange(requestEntity, Map.class);
 
-            JSONObject jsonResponse = new JSONObject(response);
-            JSONArray features = jsonResponse.getJSONArray("features");
-
-            if (features.length() > 0) {
-                coordinates = features.getJSONObject(0).getJSONObject("geometry").getJSONArray("coordinates");
-
-            }
-            conn.disconnect();
-            scanner.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            var response = responseEntity.getBody();
+            return Optional.ofNullable(response)
+                    .map(map -> (ArrayList<Map<String, Object>>) map.get("features"))
+                    .filter(features -> !features.isEmpty())
+                    .map(features -> (Map<String, Object>) features.get(0).get("geometry"));
         }
-        return Optional.of(coordinates);
+
+        return Optional.empty();
     }
 
-    public long calculateDistance(String startCity, String destinationCity) throws JSONException, IOException {
+
+    public long calculateDistance(String startCity, String destinationCity) {
         var startCityCoords = getCoordinates(startCity);
         var destinationCityCoords = getCoordinates(destinationCity);
         long distance = 0;
-        double longitudeStartCity;
-        double latitudeStartCity;
-        double longitudeDestinationCity;
-        double latitudeDestinationCity;
+
         if (startCityCoords.isPresent() && destinationCityCoords.isPresent()) {
-            longitudeStartCity = startCityCoords.get().getDouble(0);
-            latitudeStartCity = startCityCoords.get().getDouble(1);
-            longitudeDestinationCity = destinationCityCoords.get().getDouble(0);
-            latitudeDestinationCity = destinationCityCoords.get().getDouble(1);
+            Map<String, Object> startCoords = startCityCoords.get();
+            Map<String, Object> destinationCoords = destinationCityCoords.get();
+            double longitudeStartCity = (double) ((ArrayList<?>) startCoords.getOrDefault("coordinates", new ArrayList<>())).get(0);
+            double latitudeStartCity = (double) ((ArrayList<?>) startCoords.getOrDefault("coordinates", new ArrayList<>())).get(1);
+            double longitudeDestinationCity = (double) ((ArrayList<?>) destinationCoords.getOrDefault("coordinates", new ArrayList<>())).get(0);
+            double latitudeDestinationCity = (double) ((ArrayList<?>) destinationCoords.getOrDefault("coordinates", new ArrayList<>())).get(1);
+
             String urlString = "https://api.openrouteservice.org/v2/directions/driving-car?api_key=" + API_KEY
-                    + "&start=" + longitudeStartCity + "," + latitudeStartCity + "&end=" + longitudeDestinationCity + "," + latitudeDestinationCity;
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
-            Scanner scanner = new Scanner(url.openStream());
-            String responseBody = scanner.useDelimiter("\\A").next();
+                    + "&start=" + longitudeStartCity + ","
+                    + latitudeStartCity + "&end=" + longitudeDestinationCity + "," + latitudeDestinationCity;
 
 
-            String distanceString = responseBody.split("\"distance\":")[1].split(",")[0].trim();
-            scanner.close();
-            conn.disconnect();
-            return Math.round(Double.parseDouble(distanceString) / 1000);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Accept", "*/*");
+            var httpEntity = new HttpEntity("parameters", headers);
+            // headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+            // RequestEntity<?> requestEntity = new RequestEntity<>(headers, HttpMethod.GET, URI.create(urlString));
+            //ResponseEntity<Map> responseEntity = restTemplate.exchange(requestEntity, Map.class);
+            var responseEntity= restTemplate.exchange(
+                    urlString,
+                    HttpMethod.GET,
+                    httpEntity,
+                    Map.class
+            );
+            if (responseEntity.getStatusCode().is2xxSuccessful()) {
+                Map<String, Object> responseBody = responseEntity.getBody();
+                distance = Optional.ofNullable(responseBody)
+                        .map(map -> (ArrayList<Map<String, Object>>) map.get("features"))
+                        .filter(features -> !features.isEmpty())
+                        .map(features -> (Map<String, Object>) features.get(0).get("properties"))
+                        .map(properties -> (Map<String, Object>) properties.get("summary"))
+                        .map(summary -> (double) summary.get("distance"))
+                        .map(distanceInMeters -> Math.round(distanceInMeters / 1000))
+                        .orElse(distance);
+            }
         }
         return distance;
     }
